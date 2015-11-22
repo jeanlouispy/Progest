e29059bda1270c0c5c440e89ebd68a473f9d0834
jeanlouispy <jeanlouispy@hotmail.com>
2015-11-22 20:24:57 +0100

    Add Parse package + subclass Software

---
+++ Progest/Application.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Parse;
+
+namespace Progest
+{
+    public class Application
+    {
+        private Application instance;
+         
+        private Application()
+        {
+            ParseObject.RegisterSubclass <Software> ();
+            ParseClient.Initialize(ParseApplicationId, ParseWindowsKey);
+        }
+
+        public string ParseApplicationId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string ParseWindowsKey
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public Application GetInstance()
+        {
+            if (instance == null)
+                instance = new Application();
+
+            return instance;
+        }
+
+        public void Close()
+        {
+            instance = null;
+        }
+    }
+}
--- Progest/Model/Software.cs
+++ Progest/Model/Software.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Parse;
 
 namespace Progest
 {
-    public class Software
+    [ParseClassName("Software")]
+    public class Software : ParseObject
     {
         private string code;
         private string label;
         private System.Collections.Generic.List<Progest.Release> releases;
+
+        public Software()
+        {
+
+        }
 
         public Software(string code, string label)
         {
@@ -17,30 +24,19 @@ namespace Progest
             this.label = label;
         }
 
+        [ParseFieldName("code")]
         public string Code
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
+            get { return GetProperty<string>(); }
+            set { SetProperty<string>(value); }
         }
 
+        [ParseFieldName("label")]
         public string Label
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
+            get { return GetProperty<string>(); }
+            set { SetProperty<string>(value); }
         }
-
         public List<Release> Releases
         {
             get
